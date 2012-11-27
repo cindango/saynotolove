c61d9c117bd648291e032cf84927c72c93b7592f
Cindy Ho <cindango@gmail.com>
2012-11-26 22:59:49 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Saynotolove::Application.config.secret_token = 'e72a0ced40446fa888de98991fa5e39edf959f0ed79a5df59ff08376193d57e94c96ceaad2f26985dd4589ca4b58af1fd2fff13e0109dd7c352a0cccb33476ad'
